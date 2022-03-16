4e2be4edcadaaae233c72354f27c6fc17997d3ab
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-16 18:38:59 +0000

    Todos os Count's adicionados e com a documentação

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -13,6 +13,9 @@ namespace MyFirstMethod
             CountToN(8);
             CountToN(10);
 
+            /// <summary>
+            /// Contagem crescente de 1 a 10
+            /// </summary>
             static void CountTo10()
             {
                 for(int i = 1; i<=10; i++ )
@@ -20,6 +23,10 @@ namespace MyFirstMethod
                     Console.WriteLine(i);
                 }
             }
+            /// <summary>
+            /// Faz contagem crescente até ao valor indicado
+            /// </summary>
+            /// <param name="n">Valor final</param>
             static void CountToN(int n)
             {
                 for(int i = 1; i <= n; i++)
@@ -27,6 +34,22 @@ namespace MyFirstMethod
                     Console.WriteLine(i);
 
                 }
+
+            /// <summary>
+            /// Recebe dois numeros e faz contagem crescente
+            /// </summary>
+            /// <param name="n1">valor inicial</param>
+            /// <param name="n2">valor final</param>
+            static void NtoN(int n1, int n2)
+            {
+                int i;
+                for(i = n1; i<=n2; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+            NtoN(2,3);
 
             }
 
