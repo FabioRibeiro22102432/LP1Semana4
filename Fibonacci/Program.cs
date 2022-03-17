ea9443ccb33901438e098d6c75b445e20bcbaace
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-03-17 14:21:03 +0000

    Recursão com dois casos base

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        private static int Fibo(int n)
+        {
+            int FiboValue;
+            if((n == 0) ||(n == 1))
+            {
+                return n;
+            }
+            else
+            {
+                FiboValue = Fibo(n-1)+Fibo(n-2);
+                return FiboValue;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibo(8));
+        }
+    }
+}
